6ecc466e54a755470c782d88d0d23c9f56b47fb1
Frederick Johnson <fritzj96@gmail.com>
2019-09-24 03:04:31 -0700

    removed extra className jsx

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import '@ionic/core/css/core.css';
 import '@ionic/react/css/ionic.bundle.css';
 
 function App() {
-  return <div className="App"></div>;
+  return <div></div>;
 }
 
 export default App;
