703dbec9dea4164ed3db5bf1869f59b42631c487
Frederick Johnson <fritzj96@gmail.com>
2019-09-24 03:23:08 -0700

    Extracted IonMenu to it's own component

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import {
-  IonApp,
-  IonContent,
-  IonSplitPane,
-  IonMenu,
-  IonPage
-} from '@ionic/react';
+import { IonApp, IonContent, IonSplitPane, IonPage } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { Switch } from 'react-router-dom';
 
 import '@ionic/core/css/core.css';
 import '@ionic/react/css/ionic.bundle.css';
+import { Menu } from './components/Menu';
 
 function App() {
   return (
@@ -18,7 +13,7 @@ function App() {
       <IonReactRouter>
         <IonContent>
           <IonSplitPane contentId="main">
-            <IonMenu></IonMenu>
+            <Menu />
             <IonPage id="main">
               <Switch></Switch>
             </IonPage>
