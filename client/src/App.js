156083ee70e3cbd7fdfc42af23c217df20bddbc0
Frederick Johnson <fritzj96@gmail.com>
2019-09-24 03:13:22 -0700

    progress on the "Skeleton" of the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import {
+  IonApp,
+  IonContent,
+  IonSplitPane,
+  IonMenu,
+  IonPage
+} from '@ionic/react';
+import { Switch } from 'react-router-dom';
+
 import '@ionic/core/css/core.css';
 import '@ionic/react/css/ionic.bundle.css';
 
 function App() {
-  return <div></div>;
+  return (
+    <IonApp>
+      <IonContent>
+        <IonSplitPane contentId="main">
+          <IonMenu></IonMenu>
+          <IonPage id="main">
+            <Switch></Switch>
+          </IonPage>
+        </IonSplitPane>
+      </IonContent>
+    </IonApp>
+  );
 }
 
 export default App;
