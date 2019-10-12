ec355eb638cc46b7fd19afe06a5bb9fc51525a37
Frederick Johnson <fritzj96@gmail.com>
2019-10-11 19:32:22 -0700

    installed dotenv & configured it in index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,13 @@
-console.log('test');
+// I can use ES6 imports thanks to typescript transpiling them out for me.
+import express from 'express';
+
+// Renamed for clarity when I call it. "config" isn't sufficently expressive
+import { config as dotenvConfig } from 'dotenv';
+
+// Adds Environment Vars from .env file
+dotenvConfig();
+
+// Creates the express "App" so that I can do things with it.
+const app = express();
+
+app.listen(process.env.PORT);
