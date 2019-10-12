817d30288c5f5d5988e646287cd150b400df2d7d
Frederick Johnson <fritzj96@gmail.com>
2019-10-11 20:55:44 -0700

    comments

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,14 +14,14 @@
  */
 
 /*
- * This is an ES6 "import" statement. It can pull functions, classes, and even
+ * This is an "import" statement. It can pull functions, classes, and even
  * data from another file. The "express" on the left side is the variable
  * you're setting it to and the "express" string on the right is where you're
  * importing the code from (in this case, the node module named "express")
  * This one is importing the express "package" in it's entirety. (Note the
  * lack of curly brackets). For more info on import statements in ES6, read
- * this MDN page about them. Don't worry if you don't understand everything
- * yet, it's a lot to take in.
+ * this Mozilla Developer Network (MDN) page about them. Don't worry if you
+ * don't understand everything yet, it's a lot to take in.
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import
  * ----------------------------------------------------------------------------
  * You might see something more like const express = require('express') as
@@ -36,20 +36,22 @@
 import express from 'express';
 
 /*
- * This is also an ES6 import; The difference with this one is those curly
- * brackets I mentioned above. They let the user import a specific
- * part of the package or file instead of the whole thing.
+ * This is also an import statement; The difference with this one is those curly
+ * brackets I mentioned above. They denote a "named import" that lets the user
+ * import a specific part of the package or file instead of the whole thing.
+ * See the MDN import page listed above for details.
  * ----------------------------------------------------------------------------
  * In this case, I'm importing a function, but it could just as easily be a
  * class or a plain object that was exported from the package.
  * ----------------------------------------------------------------------------
  * This function creates a "connection pool" for the mongo database I'm using.
  * I'll dig into that a bit more when I actually use the function.
- *
  */
 import { createConnection } from 'typeorm';
 
-// Renamed for clarity when I call it. "config" isn't sufficently expressive.
+/* Yet another import statement. In this one, I've used the "as" keyword to
+ * rename the default
+ */
 import { config as dotenvConfig } from 'dotenv';
 
 // Adds Environment Vars from the .env shell file
