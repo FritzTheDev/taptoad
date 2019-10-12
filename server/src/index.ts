7846ef71b74825a19954c3d576341228fdce73a2
Frederick Johnson <fritzj96@gmail.com>
2019-10-11 21:34:54 -0700

    config comments

--- server/src/index.ts
+++ server/src/index.ts
@@ -36,8 +36,9 @@
  * still the only way (for now!) if you're not using a transpiler like
  * typescript or babel.
  * ----------------------------------------------------------------------------
- * For more info on JS packages, read up on what NPM is & does. This article
- * covers the basics, but there's a lot to learn.
+ * For more info on JS packages, read up on what the Node Package Manager (NPM)
+ * is & does. This article covers the basics, but there's a lot to learn.
+ * Wikipedia article on NPM
  * https://en.wikipedia.org/wiki/Npm_(software)
  */
 import express from 'express';
@@ -59,21 +60,37 @@ import { createConnection } from 'typeorm';
 
 /*
  * Yet another import statement. In this one, I've used the "as" keyword to
- * override the name the author of the package chose when they created the
- * package. There are a few reasons you might do this. Variable name overlaps,
- * for instance.
+ * override the name the author of the package chose for the function when they
+ * created the package. There are a few reasons you might do this. Multiple
+ * packages whose export names overlap are one example.
  * ----------------------------------------------------------------------------
  * In this case it's because their name, "config", isn't very specific, and
  * seeing as this file will have a lot of configuration going on, I want to be
  * as explicit as possible. It will save me & others from inevitably having to
  * see *which* configuration "config" represents.
+ * ----------------------------------------------------------------------------
+ * Again, see the above MDN link for import statements.
  */
 import { config as dotenvConfig } from 'dotenv';
 
 /*
  * Okay. Let's finally get to "Real Programming", and make some stuff happen.
- * This dotenvConfig function that we imported above
- *
+ * This dotenvConfig function that we imported above allows us to pull what
+ * are called "Environment Variables" from the .env file in our project's
+ * "root" (The project's top-level directory, where the package.json resides)
+ * This allows for us to hide things like the secret passcode we'll use to sign
+ * JSON web tokens.
+ * ----------------------------------------------------------------------------
+ * You'll DEFINITELY want to use your .gitignore file to hide
+ * these files from github, especially if your remote repository is publicly
+ * viewable. If you don't, your application will be insecure.
+ * ----------------------------------------------------------------------------
+ * Environment variables are more complex than what I've described here and you
+ * should know more about them before deploying apps to production. Read this
+ * Wikipedia article or google "Using Environment Variables in Node.js apps" to
+ * learn more. There are plenty of articles of the topic.
+ * Wikipedia article on Environment Variables:
+ * https://en.wikipedia.org/wiki/Environment_variable
  */
 dotenvConfig();
 
