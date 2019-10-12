28b7eba98462b9cd4ac439ebd6fcb1b94b8382ad
Frederick Johnson <fritzj96@gmail.com>
2019-10-11 20:24:54 -0700

    tons of commenting + imported createConnection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,33 @@
-// I can use ES6 imports thanks to typescript transpiling them out for me.
+/*
+ * index.ts
+ *
+ * Notes:
+ * This file is where a lot of the magic happens. I create, configure,
+ * and start the server here. I also create the database connection pool with
+ * TypeORM's createConnection function.
+ * -----------------------------------------------------------------------------
+ * I use express for my server framework here because it's popular & there are
+ * jobs for it, so don't get the wrong idea, this framework is dated & middling
+ * at best. It doubles as my silent revenge on code-hipsters everywhere.
+ * -----------------------------------------------------------------------------
+ */
+
+// This is an ES6 "import" statement. It can pull functions, classes, and even
+// data from another file. This one is importing the express "package"
+// in it's entirety. (Note the lack of curly brackets)
+
+// You might see something more like const express = require('express') as
+// you work with node. That's the old way to import packages in node, and still
+// the only way if you're not using a transpiler like typescript or babel.
+
+// For more info on JS packages, read up on what NPM. This article covers the
+// basics, but there's a lot to learn.
+// https://en.wikipedia.org/wiki/Npm_(software)
 import express from 'express';
+
+// This is another import statement, except this one imports a specific part of
+// the typeORM package that abstracts away nasty mongoDB query syntax for us.
+import { createConnection } from 'typeorm';
 
 // Renamed for clarity when I call it. "config" isn't sufficently expressive.
 import { config as dotenvConfig } from 'dotenv';
